d0156fd279034ce40b182ac8a016c18c16c075a6
Gabriel Serpa <gabrielmadrugaserpa08@gmail.com>
2024-11-29 10:09:57 +0000

    Add files via upload

---
+++ Atividade2exx.sql
@@ -0,0 +1,14 @@
+INSERT INTO cidades (CodigoPostal,Nome_cidades) VALUES
+(1000,"Lisboa"), 
+(9000,"Funchal"),
+(3030,"Coimbra"),
+(2900,"Setúbal"),
+(2800,"Almada"),
+(2735,"Agualva-Cacém"),
+(2605,"Queluz"),
+(4800,"Guimarães"),
+(4436,"Rio Tinto"),
+(2830,"Barreiro"),
+(2400,"Almada");    
+
+SELECT * FROM cidades;
